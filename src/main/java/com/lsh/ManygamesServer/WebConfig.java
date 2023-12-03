1ec517d5015ba6ab1407f8d0e6184c9e0c902f95
LSH3333 <dltpgustpgus@gmail.com>
2023-12-03 13:01:44 +0900

    cors 허용 origin 변경

--- src/main/java/com/lsh/ManygamesServer/WebConfig.java
+++ src/main/java/com/lsh/ManygamesServer/WebConfig.java
@@ -8,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     /**
-     * CORS 허용 
+     * CORS 허용
+     * Http response header 의 Access-Control-Allow-Origin 값에 허용하는 origin 추가함
+     * 브라우저는 서버가 보내준 위의 origin 을 확인해 응답이 유효한지 아닌지 결정
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "https://www.sehyun-portal.com/");
     }
 }
